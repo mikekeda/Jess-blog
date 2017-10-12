01a1976b256a18e3890d63aba88f6e625684dfe6
Mykhailo Keda <mkeda@softserveinc.com>
2017-10-12 12:42:21 +0300

    Prepered for CI.

---
+++ jess_blog/settings.py
@@ -0,0 +1,169 @@
+"""
+Django settings for jess_blog project.
+"""
+
+import os
+import requests
+
+SITE_ENV_PREFIX = 'JESS'
+
+
+def get_env_var(name, default=''):
+    """Get all sensitive data from google vm custom metadata."""
+    try:
+        name = '_'.join([SITE_ENV_PREFIX, name])
+        res = os.environ.get(name)
+        if res:
+            # Check env variable (Jenkins build).
+            return res
+        else:
+            res = requests.get(
+                'http://metadata.google.internal/computeMetadata/'
+                'v1/instance/attributes/{}'.format(name),
+                headers={'Metadata-Flavor': 'Google'}
+            )
+            if res.status_code == 200:
+                return res.text
+    except requests.exceptions.ConnectionError:
+        return default
+    return default
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = get_env_var(
+    'SECRET_KEY',
+    'zha7wy3q_pnfi=)h0zzi!wukd6c^x(s1z*mb-+7j)rby)q_&t7'
+)
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = bool(get_env_var('DEBUG', True))
+
+INTERNAL_IPS = (
+    '0.0.0.0',
+    '127.0.0.1',
+)
+
+ALLOWED_HOSTS = get_env_var('ALLOWED_HOSTS', '*').split(',')
+
+
+# Application definition
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+
+    'django_jenkins',
+
+    'post',
+]
+
+MIDDLEWARE_CLASSES = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF = 'jess_blog.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+                'post.context_processors.select_parent_template',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'jess_blog.wsgi.application'
+
+
+# Database
+# https://docs.djangoproject.com/en/1.10/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': get_env_var('DB_NAME', 'jess_blog'),
+        'USER': get_env_var('DB_USER', 'jess_blog_admin'),
+        'PASSWORD': get_env_var('DB_PASSWORD', 'jess_blog_pass_!_64'),
+        'HOST': get_env_var('DB_HOST', '127.0.0.1'),
+        'PORT': '',
+    }
+}
+
+# Password validation
+# https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+
+# Internationalization
+# https://docs.djangoproject.com/en/1.10/topics/i18n/
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+APPEND_SLASH = True
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.10/howto/static-files/
+
+STATICFILES_DIRS = (
+    ('', os.path.join(BASE_DIR, 'static')),
+)
+
+STATIC_ROOT = '/home/voron/sites/cdn/jkeda'
+
+STATIC_URL = 'https://cdn.mkeda.me/jkeda/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+MEDIA_URL = '/media/'
+
+JENKINS_TASKS = ('django_jenkins.tasks.run_pylint',
+                 'django_jenkins.tasks.run_pep8',
+                 'django_jenkins.tasks.run_pyflakes',)
+
+PROJECT_APPS = ['post', 'jess_blog']
